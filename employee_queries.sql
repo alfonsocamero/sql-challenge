d30206b584d812abe5d5393d81686a61e4aa3478
Alfonso Camero <alfonsogatech@gmail.com>
2021-08-10 17:54:48 -0400

    Wrote fifth query

--- employee_queries.sql
+++ employee_queries.sql
@@ -48,3 +48,12 @@ FROM employees
 INNER JOIN dept_emp USING (emp_no)
 INNER JOIN departments USING (dept_no)
 order by emp_no;
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT
+	employees.first_name,
+	employees.last_name,
+	employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
