05d7ceb2488dabc655a1146f86dd50f60c582e51
Alfonso Camero <alfonsogatech@gmail.com>
2021-08-10 17:41:38 -0400

    Wrote second query

---
+++ employee_queries.sql
@@ -0,0 +1,22 @@
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT 
+	employees.emp_no, 
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+LEFT JOIN salaries
+USING (emp_no)
+ORDER BY emp_no
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT 
+	employees.first_name,
+	employees.last_name,
+	employees.hire_date
+FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY emp_no;
